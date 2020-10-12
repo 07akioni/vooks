85e22a33f7f1e135aaa6c974d7f335e9abc4cffb
07akioni <07akioni2@gmail.com>
2020-10-13 02:29:14 +0800

    test: ignore __test__ coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts'
   ],
+  coveragePathIgnorePatterns: [
+    'src/__tests__'
+  ],
   testMatch: [
     '**/__tests__/**/*.spec.ts'
   ]
