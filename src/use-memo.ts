2207cdad4d4ddfbfebba45d15c4eb9921c31b0fe
07akioni <07akioni2@gmail.com>
2021-05-03 16:58:03 +0800

    refactor(use-memo): simplify codes

--- src/use-memo.ts
+++ src/use-memo.ts
@@ -1,4 +1,11 @@
-import { computed, ref, Ref, watch, ComputedRef, WritableComputedRef } from 'vue'
+import {
+  computed,
+  ref,
+  Ref,
+  watch,
+  ComputedRef,
+  WritableComputedRef
+} from 'vue'
 
 type MemoGetter<T> = () => T
 type MemoSetter<T> = (v: T) => void
@@ -10,30 +17,30 @@ interface WritableMemoOptions<T> {
 
 function useMemo<T> (getter: MemoGetter<T>): ComputedRef<T>
 function useMemo<T> (options: WritableMemoOptions<T>): WritableComputedRef<T>
-function useMemo<T> (getterOrOptions: MemoGetter<T> | WritableMemoOptions<T>): ComputedRef<T> | WritableComputedRef<T> {
-  const computedValueRef = computed(getterOrOptions as MemoGetter<T>)
+function useMemo<T> (
+  getterOrOptions: MemoGetter<T> | WritableMemoOptions<T>
+): ComputedRef<T> | WritableComputedRef<T> {
+  const computedValueRef = computed(getterOrOptions as any) as
+    | WritableComputedRef<T>
+    | ComputedRef<T>
   // Maybe it's not possible to lazy evaluate the value, since we can't make
   // render phase capture the deps behind useMemo
   const valueRef = ref(computedValueRef.value) as Ref<T>
-  watch(computedValueRef, value => {
+  watch(computedValueRef, (value) => {
     valueRef.value = value
   })
   if (typeof getterOrOptions === 'function') {
     return valueRef as ComputedRef<T>
   } else {
-    return new Proxy({}, {
-      get (_, key) {
-        if (key === '__v_isRef') return true
-        if (key === 'value') return valueRef.value
-        return undefined
+    return ({
+      __v_isRef: true,
+      get value () {
+        return valueRef.value
       },
-      set (_, key, v: T) {
-        if (key === 'value') {
-          getterOrOptions.set(v)
-        }
-        return true
+      set value (v: T) {
+        (getterOrOptions as WritableMemoOptions<T>).set(v)
       }
-    }) as WritableComputedRef<T>
+    } as unknown) as WritableComputedRef<T>
   }
 }
 
