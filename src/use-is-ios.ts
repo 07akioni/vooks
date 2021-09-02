1358e5644a3285cb65f7c669af9d8931b81bdc82
07akioni <07akioni2@gmail.com>
2021-09-03 03:26:14 +0800

    fix: test

--- src/use-is-ios.ts
+++ src/use-is-ios.ts
@@ -4,7 +4,7 @@ export const isIos =
     : /iPad|iPhone|iPod/.test(navigator.platform) ||
       (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  !window.MSStream
+  !(window as any).MSStream
 
 export default function useIsIos (): boolean {
   return isIos
