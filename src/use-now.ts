bc2e99c49e4e08e52befc8bb1d78cf49be57d29f
07akioni <07akioni2@gmail.com>
2021-09-25 19:21:54 +0800

    fix(now): type

--- src/use-now.ts
+++ src/use-now.ts
@@ -5,9 +5,18 @@ interface UseNowOptions {
 }
 
 // TODO, refactor it, seems the impl is not correct
-function useNow (interval: boolean | number, options: { type: 'number' }): Ref<number>
-function useNow (interval: boolean | number, options: { type: 'date' }): Ref<Date>
-function useNow (interval: boolean | number, { type = 'number' }: UseNowOptions): Ref<number | Date> {
+function useNow (
+  interval: boolean | number,
+  options: { type: 'number' }
+): Ref<number>
+function useNow (
+  interval: boolean | number,
+  options: { type: 'date' }
+): Ref<Date>
+function useNow (
+  interval: boolean | number,
+  { type = 'number' }: UseNowOptions
+): Ref<number | Date> {
   const isNumber = type === 'number'
   const nowRef = ref(isNumber ? Date.now() : new Date())
   if (interval === false) {
@@ -15,7 +24,7 @@ function useNow (interval: boolean | number, { type = 'number' }: UseNowOptions)
     onBeforeMount(() => {
       id = setInterval(() => {
         nowRef.value = isNumber ? Date.now() : new Date()
-      })
+      }) as unknown as number
     })
     onBeforeUnmount(() => {
       clearInterval(id)
